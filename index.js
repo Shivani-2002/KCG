d5a5b4d60157ca1a63e4a01320fede51d3b25398
Shivani-2002 <shivanbu2002@gmail.com>
2022-02-09 00:56:47 +0530

    commit on 'master'

---
+++ index.js
@@ -0,0 +1,118 @@
+// window.document.onkeydown = function(e) {
+//     if (!e) {
+//       e = event;
+//     }
+//     if (e.keyCode == 27) {
+//       lightbox_close();
+//     }
+//   }
+  
+//   function lightbox_open() {
+//     var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+//     window.scrollTo(0, 0);
+//     document.getElementById('light').style.display = 'block';
+//     document.getElementById('fade').style.display = 'block';
+//     lightBoxVideo.play();
+//   }
+  
+//   function lightbox_close() {
+//     var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+//     document.getElementById('light').style.display = 'none';
+//     document.getElementById('fade').style.display = 'none';
+//     lightBoxVideo.pause();
+//   }
+
+
+// var myStr="FirstLine\n\t\\SecondLine\nThirdLine";
+// console.log(myStr);
+
+
+// var count= 0;
+// function cc(val){
+//   switch(val){
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 5:
+//       count++;
+//       break;
+//     case 10:
+//     case "J":
+//     case "Q":
+//     case "K":
+//     case "A":
+//       count--;
+//       break;
+
+//   }
+//   return count;
+// }
+
+// console.log(cc(2));
+// console.log(cc('K'));
+// console.log(cc(10));
+// console.log(cc('K'));
+// console.log(cc('A'));
+// console.log(cc(4));
+
+// var myDog={name:"Doggy"};
+// console.log(myDog);
+// myDog.gender="male";
+// console.log(myDog);
+
+// function multiplyAll(arr){
+//   var product = 1;
+//   for(var i=0;i<arr.length;i++){
+//     // console.log("i is "+arr[i]);
+//     // console.log("i length is "+arr.length);
+
+//     for(var j=0;j<arr[i].length;j++){
+      
+//       product *= arr[i][j];
+//     //   console.log(arr[i][j]);
+//     //   console.log("j is "+arr[j]);
+//     //   console.log("pro is " +product);
+      
+    
+//     }
+//   }
+//   return product;
+// }
+
+// var product1=multiplyAll([[1,2],[3,4],[5,6,7]]);
+// console.log(product1);
+// console.log("hi");
+
+
+
+
+
+// program to find the largest among three numbers
+
+// take input from the user
+// const num1 = prompt("Enter first number: ");
+// const num2 = prompt("Enter second number: ");
+// const num3 = prompt("Enter third number: ");
+
+// const largest = Math.max(num1, num2, num3);
+
+// // display the result
+// console.log("The largest number is " + largest);
+
+// console.log(parseInt("10011",2));
+
+function cat(){
+  // "use strict"
+  ;
+   catname="olllli";
+   return catname;
+}
+
+console.log(cat());
+
+// var itemList =  document.getElementById('testUl');
+// var items = itemList.getElementsByTagName('li');
+// console.log(items.firstChild.textContent);
+
+// console.dir(document);
